3fd5184773299f4afeb89e6580d9d203133a9b73
Greg Stevens <152117+gastevens@users.noreply.github.com>
2025-03-15 17:20:26 -0400

    fixed application path

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,9 +2,10 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Arc.app"
-dockutil --no-restart --add "/System/Applications/Mimestream.app"
-dockutil --no-restart --add "/System/Applications/Fantastical.app"
-dockutil --no-restart --add "/System/Applications/Warp.app"
+dockutil --no-restart --add "/Applications/Mimestream.app"
+dockutil --no-restart --add "/Applications/Fantastical.app"
+dockutil --no-restart --add "/System/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Warp.app"
 dockutil --no-restart --add "/System/Applications/System Settings.app"
 
 killall Dock
